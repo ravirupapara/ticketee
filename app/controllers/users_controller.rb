5ea4559e82acf77f68da86d4da4a609a75254101
Ravi Rupapara <ravi.rupapara@aspiresoftware.in>
2015-06-04 19:20:22 +0530

    Implement profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,52 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
 
-  def create
-  	@user = User.new(user_params)
-	if @user.save
-		flash[:notice] = "You have signed up successfully."
-		redirect_to projects_path
-	else
-		render :new
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@user = User.new
 	end
-  end
 
-  def show
-  end
-
-  private
-	def user_params
-		params.require(:user).permit(:name,:password,:password_confirmation)
+	def new
+		@user = User.new
 	end
-	
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "You have signed up successfully."
+			redirect_to @user
+		else
+			render :new
+		end
+  	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+
+		if @user.save(user_params)
+			flash[:notice] = "Profile has been updated."
+			redirect_to @user
+		else
+			flash[:notice] = "Profile failed to update"
+			render :edit
+		end
+
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name,:password,:password_confirmation)
+		end
+
+		def set_user
+			@user = User.find(params[:id])
+		end
+
 end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,10 @@
+
+FactoryGirl.define do
+	
+	factory :user do
+		name "new_username"
+		email "user@example.com"
+		password_digest "password"
+	end
+	
+end
