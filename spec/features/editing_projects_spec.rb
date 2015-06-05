82666c6d41c423126fb6d776e2b3ab5a40f2c241
Ravi Rupapara <ravi.rupapara@aspiresoftware.in>
2015-06-05 20:07:41 +0530

    Restrict access to project actions to admins only

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 feature "Editing Projects" do
 
 	before do
+		sign_in_as!(FactoryGirl.create(:admin_user))
+		
 		FactoryGirl.create(:project, name: "TextMate 2")
 
 		visit "/"
